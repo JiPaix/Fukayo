2923bb04a31045da77e5c3db1c30ba95d76af93f
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-02-11 17:50:46 +0100

    add vuex example with tests

---
+++ packages/renderer/src/components/ReactiveStore.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useStore } from '/@/store/settings';
+import { MutationTypes } from '/@/store/settings/mutations';
+import { ActionTypes } from '../store/settings/actions';
+
+const settings = useStore();
+const name = ref('');
+</script>
+
+<template>
+  <p>
+    Name retrieved using getters: <b id="name">{{ settings.getters.nameWithMister }}</b>
+  </p>
+  <p>
+    Load Another name using dispatch
+    <button
+      id="load-name"
+      @click="settings.dispatch(ActionTypes.GET_NAME, 'John')"
+    >
+      LOAD
+    </button>
+  </p>
+
+  <label for="name">
+    Set Name using commit
+    <input
+      id="input-name"
+      v-model="name"
+      @input="settings.commit(MutationTypes.SET_NAME, name)"
+    >
+  </label>
+  <br>
+  <br>
+  <code>packages/renderer/src/components/ReactiveStore.vue</code>
+</template>
---
+++ packages/renderer/tests/ReactiveStore.spec.ts
@@ -0,0 +1,20 @@
+import {mount} from '@vue/test-utils';
+import {expect, test} from 'vitest';
+import ReactiveStore from '../src/components/ReactiveStore.vue';
+
+test('ReactiveHash component', async () => {
+  expect(ReactiveStore).toBeTruthy();
+  const wrapper = mount(ReactiveStore);
+
+  const name = wrapper.get('#name');
+  expect(name.text()).toBe('Mr. No name');
+
+  const loadName = wrapper.get('#load-name');
+  await loadName.trigger('click');
+  await new Promise(resolve => setTimeout(resolve, 500));
+  expect(name.text()).toBe('Mr. John');
+
+  const input = wrapper.get('#input-name');
+  await input.setValue('Smith');
+  expect(name.text()).toBe('Mr. Smith');
+});
