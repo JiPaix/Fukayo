22013db88b0ed2bfb745249dee7ecd767c18d8a6
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-02-12 12:53:38 +0100

    renamed userData to userDataPath

--- packages/renderer/src/store/settings/plugins.ts
+++ packages/renderer/src/store/settings/plugins.ts
@@ -1,10 +1,10 @@
 import type { Store } from './';
 import { MutationTypes } from './mutations';
 
-const userData = (store:Store) => {
+const userDataPath = (store:Store) => {
   window.userDataPath().then(userPath => {
     store.commit(MutationTypes.SET_USERDATAPATH, userPath);
   });
 };
 
-export const plugins = [userData];
+export const plugins = [userDataPath];
