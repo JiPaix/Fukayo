ea845e659fda55d4ac32ea21f839ec45fe516b62
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-02-28 21:36:24 +0100

    remove v-container

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -14,16 +14,13 @@
     <!-- Sizes your content based upon application components -->
     <v-main>
       <!-- Provides the application the proper gutter -->
-      <v-container
-        fluid
-      >
-        <h1>{{ $t('homepage.hello') }}</h1>
-        <fieldset>
-          <legend>Environment</legend>
-          <electron-versions v-if="win" />
-        </fieldset>
+      <h1>{{ $t('homepage.hello') }}</h1>
+      <fieldset>
+        <legend>Environment</legend>
+        <electron-versions v-if="win" />
+      </fieldset>
 
-        <!-- <h1>{{ $t('homepage.hello') }}</h1>
+      <!-- <h1>{{ $t('homepage.hello') }}</h1>
         <fieldset>
           <legend>Vuex</legend>
           <ReactiveCounter />
@@ -39,7 +36,6 @@
           Edit
           <code>packages/renderer/src/App.vue</code> to test hot module replacement.
         </p> -->
-      </v-container>
     </v-main>
   </v-app>
 </template>
