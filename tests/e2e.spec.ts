82d670880cce27d325779f0013833af4cfdbfe13
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-02-28 22:21:24 +0100

    add e2e tests for window control

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -67,3 +67,48 @@ test('Preload nodeCrypto', async () => {
   const expectedHash = createHash('sha256').update(data).digest('hex');
   expect(hash).toEqual(expectedHash);
 });
+
+test('Preload windowControl', async () => {
+  const page = await electronApp.firstWindow();
+
+  const exposedWindowControl = await page.evaluate(() => globalThis.windowControl);
+
+  expect(exposedWindowControl).to.haveOwnProperty('quit');
+  expect(exposedWindowControl).to.haveOwnProperty('minimize');
+  expect(exposedWindowControl).to.haveOwnProperty('maximize');
+
+  const quitType = await page.evaluate(() => typeof globalThis.windowControl.quit);
+  const minimizeType = await page.evaluate(() => typeof globalThis.windowControl.minimize);
+  const maximizeType = await page.evaluate(() => typeof globalThis.windowControl.maximize);
+
+  expect(quitType).toEqual('function');
+  expect(minimizeType).toEqual('function');
+  expect(maximizeType).toEqual('function');
+
+  const getMaximizedStatus = async () => {
+    return electronApp.evaluate(({BrowserWindow}) => {
+      const mainWindow = BrowserWindow.getAllWindows()[0];
+      return new Promise(resolve => {
+        setTimeout(() => resolve(mainWindow.isMaximized()), 200);
+      });
+    });
+  };
+
+  expect(await getMaximizedStatus()).toBe(false);
+  const maximize = await page.evaluate(() => globalThis.windowControl.maximize());
+  expect(await maximize).toBe(true);
+
+  const getMinimizedStatus = async () => {
+    return electronApp.evaluate(({BrowserWindow}) => {
+      const mainWindow = BrowserWindow.getAllWindows()[0];
+      return new Promise(resolve => {
+        setTimeout(() => resolve(mainWindow.isMinimized()), 200);
+      });
+    });
+  };
+
+  expect(await getMinimizedStatus()).toBe(false);
+  await page.evaluate(() => globalThis.windowControl.minimize());
+  expect(await getMinimizedStatus()).toBe(true);
+
+});
