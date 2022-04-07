1db8b81a333464b337b6ca9bb4ab172cf48ed380
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-04-07 17:05:55 +0200

    Electron removed "nativeWindowOpen" option

--- packages/main/src/mainWindow.ts
+++ packages/main/src/mainWindow.ts
@@ -7,7 +7,6 @@ async function createWindow() {
     roundedCorners: true,
     show: false, // Use 'ready-to-show' event to show window
     webPreferences: {
-      nativeWindowOpen: true,
       webviewTag: false, // The webview tag is not recommended. Consider alternatives like iframe or Electron's BrowserView. https://www.electronjs.org/docs/latest/api/webview-tag#warning
       preload: join(__dirname, '../../preload/dist/index.cjs'),
     },
