efb0283a39e3a8767d265ca1af2b9fcaaaee3b53
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-04-18 21:32:20 +0200

    feat: open search in a dialog

--- packages/renderer/src/components/mainPage.vue
+++ packages/renderer/src/components/mainPage.vue
@@ -1,48 +1,39 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import searchMangas from './searchMangas.vue';
-import type { sock } from '../socketClient';
+import { ref } from "vue";
+import searchMangas from "./searchMangas.vue";
+import type { sock } from "../socketClient";
+import { useQuasar } from "quasar";
 
 const props = defineProps<{
   logo: string;
-  socket: sock
+  socket: sock;
 }>();
+const $q = useQuasar();
 
 const leftDrawerOpen = ref(false);
+
+function openSearchDialog() {
+  $q.dialog({
+    component: searchMangas,
+    componentProps: { socket: props.socket },
+  });
+}
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header
-      elevated
-      class="bg-grey-10 text-white"
-      height-hint="98"
-    >
+    <q-header elevated class="bg-grey-10 text-white" height-hint="98">
       <q-toolbar>
-        <q-btn
-          dense
-          flat
-          round
-          icon="menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+        <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
 
         <q-toolbar-title>
-          <q-img
-            :src="props.logo"
-            width="40px"
-            height="40px"
-            fit="scale-down"
-          />
-          {{ $t('app.name.value') }}
+          <q-img :src="props.logo" width="40px" height="40px" fit="scale-down" />
+          {{ $t("app.name.value") }}
         </q-toolbar-title>
       </q-toolbar>
 
       <q-tabs align="left">
-        <q-route-tab
-          :label="$t('searchMangas.tab.value')"
-          @click="$q.dialog({component: searchMangas, componentProps: { socket }})"
-        />
+        <q-route-tab :label="$t('searchMangas.tab.value')" @click="openSearchDialog" />
       </q-tabs>
     </q-header>
 
