c0195ec008ebad2eeb6626ae165f5fc2ea3213fa
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-04-20 17:58:57 +0200

    fix: download covers for mangafox

--- packages/api/src/mirrors/mangafox.ts
+++ packages/api/src/mirrors/mangafox.ts
@@ -63,8 +63,13 @@ class Mangafox extends Mirror implements MirrorInterface {
         // safeguard
         if(!link || !isMangaLink || !name) continue;
 
-        // getting additional infos (these are all optional)
-        const cover = $('img.manga-list-4-cover', el).attr('src');
+        // mangafox images needs to be downloaded.
+        let cover:string|undefined;
+        const coverLink =  $('img.manga-list-4-cover', el).attr('src');
+        if(coverLink) {
+          const ab = await this.fetch({url: coverLink, responseType: 'arraybuffer'});
+          cover =  'data:image/jpeg;charset=utf-8;base64,'+Buffer.from(ab.data, 'binary').toString('base64');
+        }
         const last_chapter = $('p.manga-list-4-item-tip', el).filter((i,e) => $(e).text().trim().indexOf('Latest Chapter:') > -1).text().replace('Latest Chapter:', '').trim();
         let synopsis:string|undefined = $('p.manga-list-4-item-tip:last-of-type', el).text().trim();
         if(synopsis && synopsis.length === 0) synopsis = undefined
