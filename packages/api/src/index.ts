d440715675fd2503f81e54df0a5a72852bc2ddf1
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-05-12 21:36:40 +0200

    feat: API in standalone mode doesn't rely on process.env anymore
    exports useFork() which takes an object equivalent to process.env expected variables

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,44 +4,67 @@ import morgan from 'morgan';
 import { Fork } from './fork/index';
 import { verify } from './standalone';
 import client from './socket/client';
+import type { ForkEnv } from './types';
 
-// verify process.env variables are set
-verify();
+export default function useFork(settings: ForkEnv = env):Promise<client> {
+  // put settings in global scope
+  if(settings) {
+    let k:keyof typeof settings;
+    for (k in settings) {
+        if(k === 'SSL') env[k] = settings[k] as 'app' | 'provided' | 'false';
+        else env[k] = settings[k] as string;
+    }
+  }
+  // verify settings (type and value)
+  verify();
 
+  // Express config
+  const app = express();
 
-// basic express setup
-const app = express();
+  // serve the view if any
+  if(env.VIEW) {
+    app.use(express.static(env.VIEW));
+  }
 
-// serve APP
-if(env.VIEW) {
-  app.use(express.static(env.VIEW));
-}
+  // 403-404 handler
+  app.use((req, res, next) => {
+    if(res.headersSent) return next();
+    if(env.VIEW) res.status(404).json({error: 'not_found'});
+    else res.status(403).send('Forbidden');
+    next();
+  });
 
-// custom 404 page
-app.use((req, res, next) => {
-  if(res.headersSent) return next();
-  if(env.VIEW) res.status(404).json({error: 'not_found'});
-  next();
-});
+  // enable logging
+  if (env.MODE === 'development') {
+    app.use(morgan('common'));
+  } else {
+    app.use(morgan('[api] :method :url :status :response-time ms - :res[content-length]'));
+  }
 
-// enable logging
-if (env.MODE === 'development') {
-  app.use(morgan('common'));
-} else {
-  app.use(morgan('[api] :method :url :status :response-time ms - :res[content-length]'));
-}
-
-export default function useFork():Promise<client> {
+  // starting the server
   return new Promise((resolve, reject) => {
     const fork = new Fork(app);
+
+    // timeout on startup
     let timeout: NodeJS.Timeout | undefined;
-    if(!env.ELECTRON_RUN_AS_NODE) timeout = setTimeout(() => reject(new Error('timeout')), 10000);
+    // pings to keep the server alive
+    let pings:() => NodeJS.Timer | undefined;
+
+    // The ForkAPI handles that for us, but we need to do it manually when used standalone
+    if(!env.ELECTRON_RUN_AS_NODE) {
+      timeout = setTimeout(() => reject(new Error('timeout')), 10000);
+      pings = () => setInterval(() => fork.restartPingTimeout(), 5000);
+    }
+
+    // listening for the start message
     fork.once('start', ({success, message}) => {
       if(success && message) {
+        // clear timeout and start pinging if standalone
         if(!env.ELECTRON_RUN_AS_NODE) {
-          setInterval(() => fork.restartPingTimeout(), 5000);
+          if(pings) pings();
           if(timeout) clearTimeout(timeout);
         }
+        // return a socket.io client
         return resolve(new client({
           accessToken: message.split('[split]')[0],
           refreshToken: message.split('[split]')[1],
@@ -51,6 +74,7 @@ export default function useFork():Promise<client> {
       }
       return reject(new Error(message));
     });
+    // The ForkAPI handles that for us, but we need to do it manually when used standalone
     if(!env.ELECTRON_RUN_AS_NODE) {
       fork.start({
         login: env.LOGIN,
@@ -62,6 +86,7 @@ export default function useFork():Promise<client> {
         key: env.KEY,
        });
     }
+
   });
 }
 
