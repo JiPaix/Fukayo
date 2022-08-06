e18e98a23965476d025201b326e71d5c176a4f54
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-08-06 13:55:06 +0200

    test(renderer): upgrade vue-devtools version

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -45,18 +45,23 @@ app.whenReady()
   .then(restoreOrCreateWindow)
   .catch((e) => console.error('Failed create window:', e));
 
-
 /**
  * Install Vue.js or some other devtools in development mode only
  */
 if (import.meta.env.DEV) {
   app.whenReady()
     .then(() => import('electron-devtools-installer'))
-    .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
-      loadExtensionOptions: {
-        allowFileAccess: true,
+    .then(({default: installExtension}) => installExtension(
+      {
+        id: 'nhdogjmejiglipccpnnnanhbledajbpd',
+        electron: '>=18.0.2',
+      }
+      ,{
+        loadExtensionOptions: {
+          allowFileAccess: true,
+        },
       },
-    }))
+    ))
     .catch(e => console.error('Failed install extension:', e));
 }
 
