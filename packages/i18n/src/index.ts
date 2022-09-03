1f59abaf142048cc0b46f047fe1ac7e381c56018
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-09-03 02:15:14 +0200

    chore(i18n): re-export function ISO3166_1_ALPHA2_TO_ISO639_1

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,6 +1,7 @@
 import type { appLangsType, mirrorsLangsType } from '@i18n/availableLangs';
 import { appLangs, mirrorsLang } from '@i18n/availableLangs';
 import { findAppLocale } from '@i18n/findLocale';
+import { ISO3166_1_ALPHA2_TO_ISO639_1 } from '@i18n/isoConvert';
 import { importLocale, loadLocale } from '@i18n/loadLocale';
 
-export { appLangs, appLangsType, mirrorsLang, mirrorsLangsType, findAppLocale, importLocale, loadLocale };
+export { appLangs, appLangsType, mirrorsLang, mirrorsLangsType, findAppLocale, importLocale, loadLocale, ISO3166_1_ALPHA2_TO_ISO639_1 };
