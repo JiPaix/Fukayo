4191d2b101c327a192eb9f083f1bf4d2fa8a65d4
JiPaix <26584973+JiPaix@users.noreply.github.com>
2022-11-11 17:25:46 +0100

    feat(mirrors): detect if selfhosted mirror is (on/off)line
    automatically disable/enable selfhosted mirrors if they're online or not

--- packages/api/src/models/abstracts/selfhosted.ts
+++ packages/api/src/models/abstracts/selfhosted.ts
@@ -1,5 +1,6 @@
 import Mirror from '@api/models/abstracts';
 import type { MirrorConstructor } from './../types/constructor';
+import net from 'net';
 
 type options = {
   login?: string | null,
@@ -13,15 +14,46 @@ type options = {
 export class SelfHosted extends Mirror<options> {
   selfhosted = true;
   #credentialRequired: boolean;
+  #online = false;
   constructor(opts: MirrorConstructor<options>, credentialRequired:boolean) {
     super(opts);
     this.#credentialRequired = credentialRequired;
+
+    setTimeout(async () => {
+      if(!this.options.host || !this.options.port) return;
+      await this.#checkOnline(this.options.host, this.options.port);
+    }, 30*1000);
+  }
+
+  async init() {
+    const init = await super.init();
+    if(this.options.host && this.options.port) await this.#checkOnline(this.options.host, this.options.port);
+    return init;
+  }
+
+  #checkOnline(ip:string, port:number):Promise<boolean> {
+    const socket = new net.Socket();
+    socket.setTimeout(2500);
+    return new Promise(ok => {
+      const resolve = (bool:boolean) => {
+        socket.removeAllListeners();
+        socket.destroy();
+        this.logger('is', bool ? 'online':'offline');
+        this.#online = bool;
+        ok(bool);
+      };
+      socket
+        .on('connect', () => resolve(true))
+        .on('error', ()=> resolve(false))
+        .on('timeout', () => resolve(false))
+        .connect(port, ip);
+    });
   }
 
   public get enabled(): boolean {
     if(this.#credentialRequired) {
       const { enabled, host, port, password, login} = this.options;
-      if(enabled && host && port && password && login) return true;
+      if(enabled && host && port && password && login && this.#online) return true;
       return false;
     } else {
       const { enabled, host, port } = this.options;
