b1e76ad34620298cd92314236502542a50246f79
JiPaix <26584973+JiPaix@users.noreply.github.com>
2023-02-25 17:47:09 +0100

    chore(deps): weekly deps update

--- packages/api/src/utils/crawler.ts
+++ packages/api/src/utils/crawler.ts
@@ -16,7 +16,7 @@ export class Crawler {
   cluster?: Cluster<ClusterJob> = undefined;
   #puppeteer?: ReturnType<typeof addExtra> = undefined;
   #userAgent: UserAgent;
-  #revision = '1083080';
+  #revision = '1095492';
   runningTask = 0;
   #specs = { cores: 0, speedMax: 0, mem: 0 };
   constructor() {
